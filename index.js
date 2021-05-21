ebe96440477e34d1d5f8ad24bb7cc46fd126ec11
Ayoub.Gharbi <gharbi.ayoub@gmail.com>
2021-05-21 15:25:41 +0100

    add cors support

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 "use strict";
 import hapi from "@hapi/hapi";
 import { routes } from "./routes.js";
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const host = process.env.HOST || "localhost";
 const init = async () => {
   const server = hapi.server({
     port,
     host,
+    routes: {
+      cors: {
+        origin: ["*"], 
+      },
+    },
   });
   server.route(routes);
   await server.start();
